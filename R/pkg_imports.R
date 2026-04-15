ea34c1f13226f8671e41a36e008032be6077ce8c
George Chen <72078254+jiajic@users.noreply.github.com>
2026-04-15 01:02:12 -0400

    chore: imports

--- R/pkg_imports.R
+++ R/pkg_imports.R
@@ -1,8 +1,8 @@
 #' @import GiottoUtils
 #' @import tilework
 #' @import methods
-#' @importFrom terra crop ext geomtype head tail window `window<-` rasterize
-#' @importFrom GiottoClass XY calculateOverlap overlapToMatrix setGiotto createExprObj spatUnit featType prov as.terra
+#' @importFrom terra crop ext geomtype head tail window `window<-` rasterize centroids
+#' @importFrom GiottoClass XY calculateOverlap overlapToMatrix setGiotto createExprObj spatUnit featType prov as.terra createGiottoPoints createGiottoPolygon
 #' @importClassesFrom GiottoClass giotto overlapInfo
 #' @importClassesFrom terra SpatVector
 #' @importClassesFrom Matrix Matrix
